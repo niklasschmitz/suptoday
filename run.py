7b5ca2ca7cc7bfe5ad046db2979e9aa8295e3961
Niklas Schmitz <niklas.schmitz@outlook.com>
2016-12-03 17:52:40 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import requests
+import ContentRipper
+
 app = Flask(__name__)
 
 @app.route("/" , methods=['POST'])
 def incoming_message():
-
+    
     from_number = request.values.get('From', None)
     message = request.values.get('Body')
-    print(from_number + ':' + message)
+    
+    print(from_number + ':' + message) #print on the console for debugging
+    
+    
+    
     resp = twilio.twiml.Response()
-    resp.message(message)
+    reply = stringToSMS(stringhtml)
+    resp.message(reply)
     return str(resp)
 
 if __name__ == "__main__":
